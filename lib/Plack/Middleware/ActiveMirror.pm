d8ce8c3bbd4e4cacb9e74e4bd68c6c743bfe4113
Shawn M Moore <code@sartak.org>
2012-12-11 15:08:04 -0500

    More doc

--- lib/Plack/Middleware/ActiveMirror.pm
+++ lib/Plack/Middleware/ActiveMirror.pm
@@ -105,18 +105,18 @@ which means lots of pain just to load a page since each page has
 to make multiple requests to our web services.
 
 So I got to thinking, why not cache the web services responses?  As
-long as the responses form a reasonably current snapshot, it should
-work fine. Sure, I can't expect to do everything my app supports
-just with these cached responses, but at least my JavaScript loads,
-and that lets me limp along well enough to continue generating
-billable hours.
+long as the responses form a coherent, reasonably current snapshot,
+it should work fine. Sure, I can't expect to do everything my app
+supports just with these cached responses, but at least my JavaScript
+loads, and that lets me limp along well enough to continue generating
+billable hours. It's also fast as hell.
 
 I tried using off-the-shelf tools first, like the wonderful Charles
 Proxy (L<http://www.charlesproxy.com/>) and other Plack middleware,
 but none of them quite met my needs. They can mirror sets of paths
 just fine, but once you add query parameters into the mix, things
 start to go south. I needed a bit more control in what was cached,
-and how. Hence L<Plack::Middleware::ActiveMirror>.
+and how.
 
 I also wanted to make sure that in the normal case of perfect
 connectivity, my application would behave normally: every request
@@ -127,24 +127,24 @@ when connectivity goes down the drain, I can flip a switch and now
 ActiveMirror can serve responses out of cache on behalf of the
 now-inaccessible web services.
 
-=head1 SETUP
+=head1 THE CACHE
 
-L<Plack::Middleware::ActiveMirror> relies on L<CHI> to do the
-caching. This gives you enormous flexibility in how your responses
-are stored, whether in memory, on disk, in a database, whatever.
-This means that you must pass an instance of L<CHI> to get yourself
-going. (The only L<CHI> API that this module uses is C<< ->get($key)
->> and C<< ->set($key, $value) >> so if you're sneaky you can provide
-any kind of cache object -- but! I explicitly do I<not> promise that
-this module will continue using only those two methods in the future).
-
-Other than that, you should be good to go!
+L<Plack::Middleware::ActiveMirror> relies on L<CHI> to manage its
+cache. This gives you enormous flexibility in how your responses
+are stored: in memory, on disk, in a database, whatever L<CHI>
+supports. This means that you must pass an instance of L<CHI> to
+get yourself going. (The only L<CHI> API that this module uses is
+C<< ->get($key) >> and C<< ->set($key, $value) >> so if you're
+sneaky you can provide any kind of cache object -- but! I explicitly
+do I<not> promise that this module will continue using only those
+two methods with those arguments in perpetuity).
 
 =head1 OPTIONS
 
 =head2 C<cache>
 
 An initialized L<CHI> object that will hold your cached responses.
+This parameter is required.
 
 =head2 C<vary>
 
@@ -156,14 +156,17 @@ C<method> (GET, POST, etc), and C<all_parameters>.
 
 If set to a true value, then ActiveMirror will not serve any requests
 out of cache. The request will always be serviced by upstream. The
-point of this option (instead of just removing) ActiveMirror is to
-build up your cache for when you lose connectivity.
+point of this option (instead of just removing ActiveMirror) is to
+build up your cache for when you lose connectivity. So, by default,
+set C<always_fetch>, but then when you go offline, turn off
+C<always_fetch>.
 
 =head2 C<json>
 
-An instance of L<JSON> just kept around to avoid having to initialize
-it every request. You can pass in your own L<JSON> object if you
-need different options; be sure to set C<canonical>!
+An instance of L<JSON> simply kept around to avoid having to
+initialize it every request. You can pass in your own L<JSON> object
+if you need different options for some reason; be sure to set
+C<canonical>!
 
 =head1 SEE ALSO
 
